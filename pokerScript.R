c277afa87516d8b8c18983de3b7fd915a054eff6
lopez- <victorlopezse@gmail.com>
2015-05-02 02:39:20 +0100

    reading files

--- pokerScript.R
+++ pokerScript.R
@@ -1,1 +1,11 @@
 setwd("C:/Users/Victor/Google Drive/Kaggle/Poker")
+
+test <- read.csv("test.csv")
+train <- read.csv("train.csv")
+sample <- read.csv("sampleSubmission.csv")
+
+suit <- 1:4
+cards <- 1:13
+deck <- data.frame(cardId = 1:52, suit = rep(suit, each = 13), card = rep(cards, 4))
+
+# create temp deck to start taking the cards from it
